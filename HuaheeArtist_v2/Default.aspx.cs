e312adb8c1cca0ff4a94965fef7933f4c1c646f2
kazexu12 <60764235+kazexu12@users.noreply.github.com>
2022-03-13 13:33:10 +0800

    Add project files.

---
+++ HuaheeArtist_v2/Default.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace HuaheeArtist_v2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        String cs = Global.CS;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                lblMessage.Visible = false;
+                hyperlink.Visible = false;
+            }
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            HttpPostedFile postedFile = FileUpload1.PostedFile;
+            string fileName = Path.GetFileName(postedFile.FileName);
+            string fileExtension = Path.GetExtension(fileName);
+            int fileSize = postedFile.ContentLength;
+
+            if(fileExtension.ToLower() == ".jpg" || fileExtension.ToLower() == ".bmp" || fileExtension.ToLower() == ".gif" || fileExtension.ToLower() == ".png")
+            {
+                Stream stream = postedFile.InputStream;
+                BinaryReader binaryReader = new BinaryReader(stream);   
+                byte[] bytes = binaryReader.ReadBytes((int)stream.Length);
+
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand("spUploadImage", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    SqlParameter paramName = new SqlParameter()
+                    {
+                        ParameterName = "@Name",
+                        Value = fileName
+                    };
+                    cmd.Parameters.Add(paramName);
+
+                    SqlParameter paramImageData = new SqlParameter()
+                    {
+                        ParameterName = "@ImageData",
+                        Value = bytes
+                    };
+                    cmd.Parameters.Add(paramImageData);
+
+                    SqlParameter paramNewId = new SqlParameter()
+                    {
+                        ParameterName = "@NewId",
+                        Value = -1,
+                        Direction = ParameterDirection.Output
+                    };
+                    cmd.Parameters.Add(paramNewId);
+
+                    con.Open(); 
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    lblMessage.Visible = true;
+                    lblMessage.Text = "Upload Successful!";
+                    lblMessage.ForeColor = System.Drawing.Color.Green;
+                    hyperlink.Visible = true;
+                    hyperlink.NavigateUrl = "~/WebForm1.aspx?Id=" + cmd.Parameters["@NewId"].Value.ToString();
+                }
+            }
+            else
+            {
+                lblMessage.Visible = true;
+                lblMessage.Text = "Only images (.jpg, .png, .gif and .bmp) can be uploaded !";
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                hyperlink.Visible = false;
+
+            }
+        }
+    }
+}
---
+++ SQLQuery3.sql
@@ -0,0 +1,15 @@
+﻿SELECT * FROM Product
+
+CREATE PROC spUploadImage
+	@Name nvarchar(255),
+	@Size int,
+	@ImageData varbinary(max),
+	@NewId int output
+	as
+	Begin
+		Insert into [C:\USERS\ASUS\SOURCE\REPOS\HUAHEEARTIST_V2\HUAHEEARTIST_V2\APP_DATA\ARTIST.MDF]
+		values(@Name,@Size,@ImageData)
+
+		SELECT @NewId = SCOPE_IDENTITY()
+End
+
